c6c9c519d86fee44f90832163566b90811df9d48
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2024-08-28 17:18:58 -0400

    Updated build | Aug 28, 05:18PM

---
+++ assets/pages/projects/index.page.ca80d52d.js
@@ -0,0 +1,40 @@
+import{j as e}from"../../chunk-14d409af.js";const l={title:"Projects",hideTOC:!0};function t(r){const s=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p",ul:"ul",strong:"strong"},r.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(s.nav,{className:"toc",children:e.exports.jsx(s.ol,{className:"toc-level toc-level-1",children:e.exports.jsx(s.li,{className:"toc-item toc-item-h1",children:e.exports.jsx(s.a,{className:"toc-link toc-link-h1",href:"#projects",children:"Projects"})})})}),e.exports.jsxs(s.main,{children:[`
+`,e.exports.jsx(s.h1,{id:"projects",children:e.exports.jsx(s.a,{href:"#projects",children:"Projects"})}),`
+`,e.exports.jsx(s.p,{children:"CS 1230 has 6 projects + 1 final project. Each spans 2 weeks."}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.strong,{children:"Raster"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.a,{href:"/website-fall-23/projects/raster/1",children:"Project 1: Brush"}),`
+`,`
+`]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.a,{href:"/website-fall-23/projects/raster/2",children:"Project 2: Filter"}),`
+`,`
+`]}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.strong,{children:"Ray"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.a,{href:"/website-fall-23/projects/ray/1",children:"Project 3: Intersect"}),`
+`,`
+`]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.a,{href:"/website-fall-23/projects/ray/2",children:"Project 4: Illuminate"}),`
+`,`
+`]}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.strong,{children:"Realtime"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.a,{href:"/website-fall-23/projects/realtime/1",children:"Project 5: Lights, Camera"}),`
+`,`
+`]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.a,{href:"/website-fall-23/projects/realtime/2",children:"Project 6: Action!"}),`
+`,`
+`]}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.strong,{children:"Final"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/website-fall-23/projects/final",children:"Project 7: Final Project"})}),`
+`]}),`
+`]}),`
+`]})]})]})}function o(r={}){const{wrapper:s}=r.components||{};return s?e.exports.jsx(s,Object.assign({},r,{children:e.exports.jsx(t,r)})):t(r)}export{o as default,l as documentProps};
